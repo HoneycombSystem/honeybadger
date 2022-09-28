73f14e0ad050da750081b6d5cf94450a33227b2c
Mateusz Miekicki <miekickimateusz@gmail.com>
2022-09-28 18:46:41 +0200

    init commit for docs

--- cmake/enable_benchmarks.cmake
+++
@@ -1,4 +0,0 @@
-option(ENABLE_BENCHMARKS "Enable benchmarks" ON)
-if (ENABLE_BENCHMARKS)
-    add_subdirectory(${CMAKE_SOURCE_DIR}/benchmarks)
-endif ()
--- cmake/enable_docs.cmake
+++
@@ -1,4 +0,0 @@
-option(ENABLE_DOCS "Enabling the generation of documentation" ON)
-if (ENABLE_DOCS)
-    add_subdirectory(${CMAKE_SOURCE_DIR}/docs)
-endif ()
